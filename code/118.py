91967071b55a35f5ab1576bc4012a708859e50bf
unknown <1772661941@qq.com>
2020-12-11 22:06:17 +0800

    118

--- code/118.py
+++ code/118.py
@@ -1,14 +1,20 @@
 class Solution:
-	def generate(self, numRows):
-		tangle_list = [[1],[1,1]]
-		for i in range(2,numRows):
-			temp_list = []
-			temp_list.append(1)
-			for j in range(1,i):
-				temp_list.append(tangle_list[i-1][j-1]+tangle_list[i-1][j])
-			temp_list.append(1)
-			tangle_list.append(temp_list)
-		return tangle_list
+    def generate(self, numRows)
+        if numRows == 0:
+            return []
+        elif numRows == 1:
+            return [[1]]
+        elif numRows == 2:
+            return [[1],[1,1]]
+        tangle_list = [[1],[1,1]]
+        for i in range(2,numRows):
+            temp_list = []
+            temp_list.append(1)
+            for j in range(1,i):
+                temp_list.append(tangle_list[i-1][j-1]+tangle_list[i-1][j])
+            temp_list.append(1)
+            tangle_list.append(temp_list)
+        return tangle_list
 
 if __name__ == '__main__':
 	mysolution = Solution()
